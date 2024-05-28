1d83252f895e326f97dac6f45e4cad40d7dab16c
Mashy97 <104823140+Mashy97@users.noreply.github.com>
2024-05-28 12:56:30 +0900

    Add files via upload

---
+++ Class/Day27/0528_win/0528_win.cpp
@@ -0,0 +1,269 @@
+﻿// 0528_win.cpp : 애플리케이션에 대한 진입점을 정의합니다.
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "0528_win.h"
+#include <string>
+#define MAX_LOADSTRING 100
+
+// 전역 변수:
+HINSTANCE hInst;                                // 현재 인스턴스입니다.
+WCHAR szTitle[MAX_LOADSTRING];                  // 제목 표시줄 텍스트입니다.
+WCHAR szWindowClass[MAX_LOADSTRING];            // 기본 창 클래스 이름입니다.
+
+void Drawline(HDC hdc, int x, int y, int length) {
+    std::wstring wstr;
+    wstr.clear();
+    for (int i = 0; i < length; i++) {
+        wstr.insert(0, L"*");
+    }
+    TextOut(hdc, x, y, wstr.data(), (int)wstr.size());
+}
+
+void Drawline_2(HDC hdc, int x, int y, int length) {
+    std::wstring wstr = L"*";
+    for (int i = 0; i < length; i++) {
+        TextOut(hdc, x, y + (i*10), wstr.data(), (int)wstr.size());
+    }
+}
+
+void wstr_Rect(HDC hdc, int x, int y, int length) {
+    int height = (length / 2);
+    Drawline(hdc, x, y, length);
+    Drawline_2(hdc, x, y, height + 1);
+    Drawline(hdc, x, y + (height) * 10, length);
+    Drawline_2(hdc, x + (height + 1) * 10, y, height + 1);
+}
+
+
+// 이 코드 모듈에 포함된 함수의 선언을 전달합니다:
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPWSTR    lpCmdLine,
+                     _In_ int       nCmdShow)
+{
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+
+    // TODO: 여기에 코드를 입력합니다.
+
+    // 전역 문자열을 초기화합니다.
+    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+    LoadStringW(hInstance, IDC_MY0528WIN, szWindowClass, MAX_LOADSTRING);
+    MyRegisterClass(hInstance);
+
+    // 애플리케이션 초기화를 수행합니다:
+    if (!InitInstance (hInstance, nCmdShow))
+    {
+        return FALSE;
+    }
+
+    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_MY0528WIN));
+
+    MSG msg;
+
+    // 기본 메시지 루프입니다:
+    while (GetMessage(&msg, nullptr, 0, 0))
+    {
+        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+    }
+
+    return (int) msg.wParam;
+}
+
+
+
+//
+//  함수: MyRegisterClass()
+//
+//  용도: 창 클래스를 등록합니다.
+//
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+    WNDCLASSEXW wcex;
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+
+    wcex.style          = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc    = WndProc;
+    wcex.cbClsExtra     = 0;
+    wcex.cbWndExtra     = 0;
+    wcex.hInstance      = hInstance;
+    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_MY0528WIN));
+    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
+    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_MY0528WIN);
+    wcex.lpszClassName  = szWindowClass;
+    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+    return RegisterClassExW(&wcex);
+}
+
+//
+//   함수: InitInstance(HINSTANCE, int)
+//
+//   용도: 인스턴스 핸들을 저장하고 주 창을 만듭니다.
+//
+//   주석:
+//
+//        이 함수를 통해 인스턴스 핸들을 전역 변수에 저장하고
+//        주 프로그램 창을 만든 다음 표시합니다.
+//
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+   hInst = hInstance; // 인스턴스 핸들을 전역 변수에 저장합니다.
+
+   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+      0, 0, 1280, 600, nullptr, nullptr, hInstance, nullptr);
+
+   if (!hWnd)
+   {
+      return FALSE;
+   }
+
+   ShowWindow(hWnd, nCmdShow);
+   UpdateWindow(hWnd);
+
+   return TRUE;
+}
+
+//
+//  함수: WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+//  용도: 주 창의 메시지를 처리합니다.
+//
+//  WM_COMMAND  - 애플리케이션 메뉴를 처리합니다.
+//  WM_PAINT    - 주 창을 그립니다.
+//  WM_DESTROY  - 종료 메시지를 게시하고 반환합니다.
+//
+//
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+
+    switch (message)
+    {
+        
+    case WM_COMMAND:
+        {
+            int wmId = LOWORD(wParam);
+            // 메뉴 선택을 구문 분석합니다:
+            switch (wmId)
+            {
+            case IDM_ABOUT:
+                DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+                break;
+            case IDM_EXIT:
+                DestroyWindow(hWnd);
+                break;
+            default:
+                return DefWindowProc(hWnd, message, wParam, lParam);
+            }
+        }
+        break;
+        
+    case WM_LBUTTONDOWN:
+        break;
+    case WM_LBUTTONUP:
+        break;
+    case WM_MOUSEMOVE:
+    {
+        HDC hdca = GetDC(hWnd);
+        std::wstring mwstr = L"*";
+        int endX = LOWORD(lParam);
+        int endY = HIWORD(lParam);
+        TextOut(hdca, endX, endY, mwstr.c_str(), (int)mwstr.size());
+
+        ReleaseDC(hWnd, hdca);
+        break;
+    }
+    case WM_PAINT:
+        {
+            PAINTSTRUCT ps;
+            HDC hdc = BeginPaint(hWnd, &ps);
+            // TODO: 여기에 hdc를 사용하는 그리기 코드를 추가합니다...
+            FillRect(hdc,&ps.rcPaint,(HBRUSH)(COLOR_WINDOW +1));
+
+
+            // 펜과 브러시 생성
+            HPEN hPen = CreatePen(PS_SOLID, 5, RGB(0, 0, 125));
+            HBRUSH hBrush = CreateSolidBrush(RGB(75,125, 0));
+
+            // 펜과 브러시 HDC에 적용
+            HPEN hOldPen = (HPEN)SelectObject(hdc, hPen);
+            HBRUSH hOldBrush = (HBRUSH)SelectObject(hdc, hBrush);
+
+
+            // 모서리가 둥근 사각형
+            RoundRect(hdc, 50, 50, 200, 200, 20, 20);
+
+            // 원
+            Ellipse(hdc, 50, 50, 200, 200);
+          
+            // 사각형
+            Rectangle(hdc, 75, 75, 175, 175);
+
+            // HDC Text 색상 설정
+            SetTextColor(hdc, RGB(255, 125, 0));
+
+            // 호
+            Arc(hdc, 75, 75, 175, 175, 225, 225, 275, 275);
+            
+            // 파이 그리기
+            Pie(hdc, 450, 50, 600, 200, 500, 50, 550, 400);
+            
+            // 코드 - 타원의 호와 그 호를 연결하는 선분으로 둘러쌓인 영역
+            Chord(hdc, 450, 250, 600, 400, 500, 250, 550, 400);
+
+            // 선 그리기
+            MoveToEx(hdc, 500, 500, NULL);  // 시작점
+            LineTo(hdc, 500, 400);  // 끝점
+            
+            // 문자(wstr) 가로 출력
+            Drawline(hdc, 200, 200, 10);
+            // 문자(wstr) 세로 출력
+            Drawline_2(hdc, 250, 250, 6);
+
+            // 문자(wstr) 사각형 출력
+            wstr_Rect(hdc, 300, 300, 20);
+
+            EndPaint(hWnd, &ps);
+        }
+        break;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
+}
+
+// 정보 대화 상자의 메시지 처리기입니다.
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    UNREFERENCED_PARAMETER(lParam);
+    switch (message)
+    {
+    case WM_INITDIALOG:
+        return (INT_PTR)TRUE;
+
+    case WM_COMMAND:
+        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+        {
+            EndDialog(hDlg, LOWORD(wParam));
+            return (INT_PTR)TRUE;
+        }
+        break;
+    }
+    return (INT_PTR)FALSE;
+}
