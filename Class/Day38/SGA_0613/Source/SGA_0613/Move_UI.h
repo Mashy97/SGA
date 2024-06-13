32e342130d8daea12bd41d95303b59ca6b121232
Mashy97 <104823140+Mashy97@users.noreply.github.com>
2024-06-13 13:46:19 +0900

    Add files via upload

---
+++ Class/Day38/SGA_0613/Source/SGA_0613/Move_UI.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "Move_UI.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SGA_0613_API UMove_UI : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+public:
+	UMove_UI() {};
+
+};
